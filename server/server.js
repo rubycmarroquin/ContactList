14c222d75fa4276d2315111cfcf4885b92e24651
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-03-29 22:17:01 -0700

    created new api call to fetch users

--- server/server.js
+++ server/server.js
@@ -14,9 +14,22 @@ app.get("/", (req, res) => {
   res.json({ message: "Hola, from My template ExpressJS with React-Vite" });
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    const { rows: users } = await db.query(
+      "select username from users order by user_id"
+    );
+    res.send(users);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
 app.get("/contacts", async (req, res) => {
   try {
-    const { rows: contacts } = await db.query("select * from contacts");
+    const { rows: contacts } = await db.query(
+      "SELECT users.username, contacts.contact_id, contacts.name, contacts.email, contacts.phone, contacts.notes, contacts.icon FROM contacts INNER JOIN users on contacts.user_id = users.user_id"
+    );
     res.send(contacts);
   } catch (e) {
     return res.status(400).json({ e });
@@ -25,32 +38,34 @@ app.get("/contacts", async (req, res) => {
 
 // create the POST request
 app.post("/contacts", async (req, res) => {
-  try {
-    const newContact = {
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      notes: req.body.notes,
-      user_id: req.body.user_id,
-    };
+    try {
+      const newContact = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        notes: req.body.notes,
+        user_id: req.body.user_id,
+        icon: req.body.icon,
+      };
 
-    //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
-    const result = await db.query(
-      `INSERT INTO contacts(contact_id, name, email, phone, notes, user_id) VALUES(nextval('contacts_sequence'), $1, $2, $3, $4, $5) RETURNING *`,
-      [
-        newContact.name,
-        newContact.email,
-        newContact.phone,
-        newContact.notes,
-        newContact.user_id,
-      ]
-    );
-    console.log(result.rows[0]);
-    res.json(result.rows[0]);
-  } catch (e) {
-    console.log(e);
-    return res.status(400).json({ e });
-  }
+      //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
+      const result = await db.query(
+        `INSERT INTO contacts(contact_id, name, email, phone, notes, user_id, icon) VALUES(nextval('contacts_sequence'), $1, $2, $3, $4, $5, $6) RETURNING *`,
+        [
+          newContact.name,
+          newContact.email,
+          newContact.phone,
+          newContact.notes,
+          newContact.user_id,
+          newContact.icon,
+        ]
+      );
+      console.log(result.rows[0]);
+      res.json(result.rows[0]);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ e });
+    }
 });
 
 // delete request for students
@@ -68,7 +83,6 @@ app.delete("/contacts/:contactId", async (req, res) => {
 
 //A put request - Update a student
 app.put("/contacts/:contactId", async (req, res) => {
-  //console.log(req.params);
   //This will be the id that I want to find in the DB - the student to be updated
   const contactId = req.params.contactId;
   const updatedContact = {
@@ -76,6 +90,8 @@ app.put("/contacts/:contactId", async (req, res) => {
     email: req.body.email,
     phone: req.body.phone,
     notes: req.body.notes,
+    user_id: req.body.user_id,
+    icon: req.body.icon,
   };
   console.log("In the server from the url - the student id", contactId);
   console.log(
@@ -83,12 +99,14 @@ app.put("/contacts/:contactId", async (req, res) => {
     updatedContact
   );
   // UPDATE students SET lastname = "something" WHERE id="16";
-  const query = `UPDATE contacts SET name=$1, email=$2, phone=$3, notes=$4 WHERE contact_id=${contactId} RETURNING *`;
+  const query = `UPDATE contacts SET name=$1, email=$2, phone=$3, notes=$4, icon=$5, user_id=$6 WHERE contact_id=${contactId} RETURNING *`;
   const values = [
     updatedContact.name,
     updatedContact.email,
     updatedContact.phone,
     updatedContact.notes,
+    updatedContact.icon,
+    updatedContact.user_id,
   ];
   try {
     const updated = await db.query(query, values);
